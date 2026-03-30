1a1df62d093fd60527f2103f939af0bbde48ee57
Emmanuel555 <emmanueltang7@gmial.com>
2026-03-30 18:17:58 +0800

    settle w computer tmr

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,61 @@
 #include <Arduino.h>
 
+// const int TEST_PIN = 8;   // change this to the GPIO you want to test
+
+//#define RXD2 8
+//#define TXD2 7
+
+// for some reason, the gpio below follows the pin numbering of the ESP32-S3-DevKitC-1, which is different from the Schematics.
+static const int RXD2 = 44;
+static const int TXD2 = 43;  
+
 void setup(){
-    Serial.begin(115200);     // PC monitor
+    Serial.begin(115200);  // initialize serial connection to PC monitor
     
-    // Give USB time
-    delay(1000);
+    // Give PC time to open up the serial connection/monitor before we start sending data, otherwise the first few messages may be missed.
+    delay(5000);
+
+    //Serial.println("Starting GPIO output test");
+    //pinMode(TEST_PIN, OUTPUT);  // set the test pin as an output
 
     Serial.println("ESP32 starting...");
 
     // Now safe to init UART
-    // Serial2.begin(115200);
+    // UART to Teensy
+    Serial1.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    delay(1000);   
 }
 
 void loop(){
-    // Send message
-    Serial.println("hello");
+    
+    Serial1.println("hello");
+
+    delay(50);
+
+    if (Serial1.available()) {
+        //Serial.println("Printing: ");
+        char c = Serial1.read();
+        Serial.write(c);
+    }
+    
+    delay(50);
+}
+    
+    
+    
+    //digitalWrite(TEST_PIN, HIGH);
+    //Serial.println("HIGH");
+    //delay(1000);
+
+    //digitalWrite(TEST_PIN, LOW);
+    //Serial.println("LOW");
+    //delay(1000);
+
+
+
+
+// Send message
+    // Serial.println("hello");
 
     // // Wait a bit
     // delay(500);
@@ -26,5 +67,4 @@ void loop(){
     // }
 
     // Serial.println();
-    delay(1000);
-}
+    //delay(1000);
